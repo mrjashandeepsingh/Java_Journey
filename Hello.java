05b78f9becb61190812fdce0b70479d62ba74101
mrjashandeepsingh <mrjashandeepsingh1@gmail.com>
2026-01-11 20:55:07 +0530

    Output and Variables

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        int num1 = 3;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        
+    }
+}
